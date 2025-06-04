3df583aa32be871fdfc9acd657db361b9a911f45
flokingway <1206005655@qq.com>
2025-06-04 07:36:37 +0000

    init

--- src/llamafactory/webui/interface.py
+++ src/llamafactory/webui/interface.py
@@ -94,7 +94,7 @@ def run_web_ui() -> None:
     server_name = os.getenv("GRADIO_SERVER_NAME", "[::]" if gradio_ipv6 else "0.0.0.0")
     print("Visit http://ip:port for Web UI, e.g., http://127.0.0.1:7860")
     fix_proxy(ipv6_enabled=gradio_ipv6)
-    create_ui().queue().launch(share=gradio_share, server_name=server_name, inbrowser=True)
+    create_ui().queue().launch(share=True, server_name=server_name, inbrowser=True)
 
 
 def run_web_demo() -> None:
@@ -103,4 +103,4 @@ def run_web_demo() -> None:
     server_name = os.getenv("GRADIO_SERVER_NAME", "[::]" if gradio_ipv6 else "0.0.0.0")
     print("Visit http://ip:port for Web UI, e.g., http://127.0.0.1:7860")
     fix_proxy(ipv6_enabled=gradio_ipv6)
-    create_web_demo().queue().launch(share=gradio_share, server_name=server_name, inbrowser=True)
+    create_web_demo().queue().launch(share=True, server_name=server_name, inbrowser=True)
